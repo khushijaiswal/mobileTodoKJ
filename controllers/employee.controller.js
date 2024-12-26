a9785185cb46b0bcd76c0a1978325075f0b97046
khushijaiswal <jaiswalkhushi9011@gmail.com>
2024-12-26 08:58:26 +0530

    updated employee controller

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -3,7 +3,7 @@ const Todo = require("../models/Todo")
 
 exports.readEmployeeTodo = asyncHandler(async (req, res) => {
     const result = await Todo.find({ employee: req.employee })  //reslt mein jo login hai uske todos rahege sare //req.employee middleware ki file se ara hai
-    res.json({ message: "todo fetch success" }, result)
+    res.json({ message: "todo fetch success", result })
 })
 exports.completeEmployeeTodo = asyncHandler(async (req, res) => {
     await Todo.findByIdAndUpdate(req.params.tid, { isComplete: true })
